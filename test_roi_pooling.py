350cd9531b52bc9e3bd70859dcb8e5faab193b62
Praburam Krishnaswamy varadharajan <85429643+prabu-ram@users.noreply.github.com>
2024-11-09 11:54:29 -0800

    Update test_roi_pooling.py

--- test_roi_pooling.py
+++ test_roi_pooling.py
@@ -5,10 +5,9 @@ from compiler import *
 from roi_pooling import *
 from torchvision.ops import roi_pool
 
-# Allow ROI pooling op to run through CPU fallback if it's not implemented.
 os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
 
-# Define the MPS device
+#  MPS device
 if torch.backends.mps.is_available():
     mps_device = torch.device("mps")
 else:
@@ -18,7 +17,6 @@ def test_speedup():
     custom_mps_roipooling = 0
     default_roipooling = 0
     
-    # Use float32 for feature_map
     feature_map = torch.randn(1, 3, 32, 32, dtype=torch.float32, device=mps_device)
     rois = torch.tensor([[0, 0, 0, 16, 16]], dtype=torch.float32, device=mps_device)
 
@@ -28,7 +26,7 @@ def test_speedup():
     custom_model = CustomMPSROIPoolingModel().to(mps_device)
     torchvision_model = TorchvisionROIPoolingModel().to(mps_device)
 
-    # Measure time
+    # time
     for _ in range(100):
         start = time.time()
         torchvision_model.forward(feature_map, rois)
@@ -53,26 +51,24 @@ def test_correctness():
     print("Feature Map Type:", feature_map.dtype)
     print("ROIs Type:", rois.dtype)
 
-    # Perform ROI pooling using the default and custom implementations
     default_roipooling_op = roi_pool(feature_map, rois, output_size=(7, 7))
     custom_roipooling_op = custom_roipooling(feature_map, rois)
 
-    # Debug output to compare results
+    # compare results
     print("Default ROI Pooling Output:\n", default_roipooling_op)
     print("Custom ROI Pooling Output:\n", custom_roipooling_op)
 
-    # Print the first few elements to check xstart, ystart, xend, yend if relevant
-    if custom_roipooling_op.numel() > 0:  # Check if output is non-empty
+   
+    if custom_roipooling_op.numel() > 0: 
         print("Debug pooled features (first few elements):", custom_roipooling_op[0, 0, 0, :4])
 
-    # Check if the two outputs are close
+  
     torch.testing.assert_close(custom_roipooling_op, default_roipooling_op)
 
-    # Check if the two outputs are close
     torch.testing.assert_close(custom_roipooling_op, default_roipooling_op)
 
 
-# Run both tests for correctness and speedup.
+# correctness and speedup.
 def test_roi_pooling():
     test_correctness()
     test_speedup()
